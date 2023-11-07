1727800169345932657c2b55f0b910d851d80c0e
I-M-Marinov <lcfrrr@gmail.com>
2023-11-07 23:56:45 +0200

    Add project files.

---
+++ P1.SquareRoot/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+try
+{
+    if (number < 0)
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+    Console.WriteLine($"{Math.Sqrt(number)}");
+}
+catch (ArgumentException argEx)
+{
+    Console.WriteLine($"{argEx.Message}");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
---
+++ P2.EnterNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿void ReadNumber(int start, int end)
+{
+    List<int> numbersList = new();
+
+    while (numbersList.Count != 10)
+    {
+        
+        string input = Console.ReadLine();
+        
+        try
+        {
+            bool isNumber = int.TryParse(input, out int currentNumber);
+
+            if (!isNumber)
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+
+            if ((numbersList.Count == 0 && currentNumber <= 1) || currentNumber >= 100)
+            {
+                throw new ArgumentException("Your number is not in range 1 - 100!");
+            }
+
+            if (numbersList.Count > 0 && numbersList.Last() >= currentNumber || currentNumber >= 100)
+            {
+                throw new ArgumentException($"Your number is not in range {numbersList.Last()} - 100!");
+            }
+
+            numbersList.Add(currentNumber);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    Console.WriteLine(string.Join(", ", numbersList));
+}
+
+ReadNumber(1, 100);
+
